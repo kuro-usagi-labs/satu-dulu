851a81b3e0656cb762bd00d74936753b24ca41e9
kuro-usagi-labs <kusagi.gf@gmail.com>
2026-07-17 23:05:49 +0700

    fix: disambiguate Drift and matcher null helpers

--- test/tracker_repository_test.dart
+++ test/tracker_repository_test.dart
@@ -1,4 +1,4 @@
-import 'package:drift/drift.dart';
+import 'package:drift/drift.dart' hide isNotNull, isNull;
 import 'package:drift/native.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:satu_dulu/core/database/app_database.dart';
